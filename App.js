f1a3a9f3ff68bfbda34092e48d103ff190af467e
nataliafonsecaa <natalia.fonseca8@etec.sp.gov.br>
2023-05-29 20:52:56 -0300

    aula apostila 04
    aula 29/05

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
+
+
 import Apostila_03 from './components/apostila_3';
+import Apostila_04 from './components/apostila04';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila_03/>
+      <Apostila_04/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +19,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems:'center',
      //paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#9E9E9E',
+    backgroundColor: '#FFF',
     padding: 8,
   },
 });
--- components/apostila_3/index.js
+++ components/apostila_3/index.js
@@ -1,73 +1,67 @@
-import React, {useState} from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native'; 
 
 import styles from './styles';
 
 export default function Index() {
-    
+
     const [numero, setNumero] = useState(0);
 
+    function addNumber(){
+        setNumero(numero + 1);
+    } 
 
-    function addNumber (){
-      setNumero(numero + 1);
+    function subNumber(){
+        setNumero(numero - 1);
     }
 
-    function removeNumber(){
-      setNumero(numero - 1);
-    }
-
-    function zeroNumber(){
-      setNumero(0);
+    function zerar(){
+        setNumero(0);
     }
 
   return(
     <View style={styles.container}>
         <Text style={styles.paragraph}>
-            EXEMPLO 3
-        </Text>  
+            Exemplo 3
+        </Text>            
 
         <View style={styles.programa}>
-        <View style={styles.marginHorizontal}>
-            
-            <Text style={styles.textCounter}>{numero}</Text>
-    
-            <TouchableOpacity 
-             style={styles.button1}
-             onPress={() => addNumber()}
-       
-            >
-                <Text style={styles.textButton}> 
-                    +1
-                </Text>
-            </TouchableOpacity>
-                <Text style={styles.textButton}> 
-                    0
-                </Text>
-            <TouchableOpacity 
-             style={styles.button2}
-             onPress={() => removeNumber()}
-            >
-            
-                <Text style={styles.textButton}> 
-                    -1
-                </Text>
-            </TouchableOpacity>
+
+            <View style={styles.horizontal}>
+
+                <TouchableOpacity 
+                    style={[styles.button, styles.buttonMaisMenos]} 
+                    onPress={() => subNumber()}
+                >
+                    <Text style={styles.textButton}>
+                        -
+                    </Text>
+                </TouchableOpacity>
+
+                <Text style={styles.textCounter}>{numero}</Text>
+
+                <TouchableOpacity 
+                    style={[styles.button, styles.buttonMaisMenos]} 
+                    onPress={() => addNumber()}
+                >
+                    <Text style={styles.textButton}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+
             </View>
-    
+
             <TouchableOpacity 
-             style={styles.button3}
-             onPress={() => zeroNumber()}
+            style={[styles.button, {borderRadius: 30}]} 
+            onPress={() => zerar()}
             >
-              
-                <Text style={styles.textButton}> 
-                    0
+                <Text style={styles.textButton}>
+                    Zerar
                 </Text>
-            </TouchableOpacity>
-        
-        </View>
-        </View>
+            </TouchableOpacity>        
 
+        </View>                
 
-          
+    </View>
   );
 }
